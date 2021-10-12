e1620d8f62cfbdd4996ce94d7966d11c2ed1ec38
Atanu <guptaatanu91@gmail.com>
2021-10-12 14:04:10 +0000

    test

--- migrationAutomation/postSqldbCutoverSharepointServer/scripts/powershell/postSqldbCutoverSharepointServer.ps1
+++ migrationAutomation/postSqldbCutoverSharepointServer/scripts/powershell/postSqldbCutoverSharepointServer.ps1
@@ -37,13 +37,13 @@ dir
 
 Write-Host '>> DB Query Result <<'  
 
-Write-Host "Query 1:"
-./SQLCMD.EXE -S "$servername" -d "$database" -U "$username" -P "$password" -W -Q "select @@VERSION"
-statusCheck 
-
-# Write-Host "Query 2:"
-# ./SQLCMD.EXE -S "10.0.0.185" -d "testdb" -U "sa" -P "fPAgCohVU!" -W -Q "select @@VERSION"
+# Write-Host "Query 1:"
+# ./SQLCMD.EXE -S "$servername" -d "$database" -U "$username" -P "$password" -W -Q "select @@VERSION"
 # statusCheck 
+
+Write-Host "Query 2:"
+./SQLCMD.EXE -S "10.0.0.185" -d "testdb" -U "sa" -P "fPAgCohVU!" -W -Q "select @@VERSION"
+statusCheck 
 
 #Write-Host "Query 1:"
 #SQLCMD.exe -S "$servername" -d "$database" -U "$username" -P "$password" -W -Q "UPDATE [XPRESSHR_GLOBAL].[dbo].[GlobalConfig] SET Value='$urlOfEp' where [Key]='EmployeePortal.ExternalUrlBase'"
